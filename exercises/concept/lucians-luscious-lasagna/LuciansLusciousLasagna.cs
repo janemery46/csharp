674df5310d96abee490857546b34d59aefdb5703
Jan Emery Florendo <84490618+janemery46@users.noreply.github.com>
2024-02-16 07:42:59 +0800

    Completed Lucian's Luscious Lasagna Exercise

--- exercises/concept/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ exercises/concept/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,27 @@
 class Lasagna
 {
     // TODO: define the 'ExpectedMinutesInOven()' method
-
+    static int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
     // TODO: define the 'RemainingMinutesInOven()' method
+    static int RemainingMinutesInOven(int actualMins)
+    {
+        int expectedMins = ExpectedMinutesInOven();
+        return expectedMins - actualMins;
+    }
 
     // TODO: define the 'PreparationTimeInMinutes()' method
-
+    static int PreparationTimeInMinutes(int layers)
+    {
+        return layers * 2;
+    }
+        
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    static int ElapsedTimeInMinutes(int layers, int actual)
+    {
+        int prepTime = PreparationTimeInMinutes(layers);
+        return prepTime + actual;
+    }
 }
