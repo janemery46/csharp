475b76739eb59b28c571a92895b7bdcc613845a5
Jan Emery Florendo <84490618+janemery46@users.noreply.github.com>
2024-02-16 12:58:55 +0800

    Squeaky Clean Exercise Completed

--- exercises/concept/squeaky-clean/SqueakyClean.cs
+++ exercises/concept/squeaky-clean/SqueakyClean.cs
@@ -4,6 +4,11 @@ public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        identifier = Regex.Replace(identifier, " ", "_");
+        identifier = Regex.Replace(identifier, "\0", "CTRL");
+        identifier = Regex.Replace(identifier, @"-\D", new MatchEvaluator(CamelCasing));
+        return Regex.Replace(identifier, @"[α-ω]|\d|\W", "");    
     }
+
+    private static string CamelCasing(Match match) => match.Value[1].ToString().ToUpper();
 }
